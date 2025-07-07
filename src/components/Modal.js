895a5d78b208dae07b091af1e1e9a0484f8cd466
Jonathan Deininger <J_Deininger@web.de>
2025-07-07 17:41:46 +0200

    test2

--- src/components/Modal.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import "./Modal.css"; // Ensure Modal.css is imported
-
-const Modal = ({ children }) => {
-  return <div className="modal-overlay">{children}</div>;
-};
-
-export default Modal;
